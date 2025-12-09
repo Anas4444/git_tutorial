d14603ec0908e46a40afc991ee3c491bc3969349
anas4444 <anas.el.abed.official@gmail.com>
2025-12-09 19:56:33 +0100

    changing index.js message to gitflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function main() {
-    console.log(`Welcome to the Git tutorial! this is a secret: ${process.env.SECRET}`);
+    console.log(`Welcome to the Git flow tutorial! this is a secret: ${process.env.SECRET}`);
 }
 
 main();
